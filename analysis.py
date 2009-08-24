e5c9fb8381dc7a4d9c6d9f9f605584d78f4c570f
Psyche Loui <psycheloui@gmail.com>
2009-08-24 11:18:01 -0400

    testing
    new file

--- analysis.py
+++ analysis.py
@@ -2,3 +2,6 @@
 
 import csv
 
+data = csv.reader(open('../../data/emo mus/phase 2 timeseries/timeseries smooth 10/sm10_ts_aalb1094s1_01-00-063-i-bettemidler-windbeneathmywings.txt'), delimiter=',')
+for row in data:
+...	print ', '.join(row)
